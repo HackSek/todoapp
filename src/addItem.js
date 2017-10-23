f73b4d0a81554e39d617c79f9af5fc017457a118
Hachem <HackSek@users.noreply.github.com>
2017-10-23 21:22:31 +0100

    First commit

---
+++ src/addItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './addItem.css';
+
+
+export class AddItem extends Component {
+
+   constructor(props) {
+
+     super(props);
+     this.handleSubmit = this.handleSubmit.bind(this);
+
+   }
+
+   render() {
+
+    return(
+
+      <form id="add-todo" onSubmit={this.handleSubmit}>
+
+       <input type="text" required ref="newItem" />
+       <input type="submit" value="Ajouter" />
+
+      </form>
+
+    );
+
+   }
+
+   handleSubmit(e) {
+
+     e.preventDefault();
+     this.props.onAdd(this.refs.newItem.value);
+     document.getElementById("add-todo").reset();
+
+   }
+
+
+ 
+
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoComponent from './App';
+
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+registerServiceWorker();
